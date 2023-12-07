83ed36a7e0142724683db89b2e705e448e844bb0
zuirseime <vovakibets88@gmail.com>
2023-12-07 20:07:31 +0200

    Created project

---
+++ main.py
@@ -0,0 +1,54 @@
+import pygame
+from globals import *
+from world import World
+from sprite import Sprite
+
+
+class Simulator:
+    def __init__(self) -> None:
+        pygame.init()
+        pygame.font.init()
+        pygame.display.set_caption("ЛР6 Кібець")
+
+        self.screen = pygame.display.set_mode(SCREEN_SIZE)
+        self.clock = pygame.time.Clock()
+        self.font = pygame.font.SysFont(None, 30)
+
+        Sprite.gen_textures()
+        
+        self.running = True
+
+        self.world = World(MAP_SIZE, PREDATOR_NUMBER, PREY_NUMBER)
+
+
+    def run(self):
+        while self.running:
+            self.update()
+            self.draw()
+        
+        self.close()
+
+
+    def update(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+        self.screen.fill((173, 216, 230))
+
+        self.world.update(self.clock.tick(FPS))
+
+
+    def draw(self):
+        self.world.draw(self.screen)
+
+        pygame.display.flip()
+
+
+    def close(self):
+        pygame.quit()
+
+
+if __name__ == "__main__":
+    simulator = Simulator()
+    simulator.run()
